a9a7f465a18f50c5df34e9899c8c84d4af561056
costindumitrescu <dumitrescucostin20@gmail.com>
2020-03-27 12:08:20 -0400

    done ish

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-$('.menu-toggler').on('click', function() {
-  $(this).toggleClass('open');
-  $('.top-nav').toggleClass('open');
-});
+$(document).ready(() => {
+  $('.menu-toggler').on('click', function() {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
 
 $('.top-nav .nav-link').on('click', function() {
   $('.menu-toggler').removeClass('open');
@@ -12,13 +12,13 @@ $('.top-nav .nav-link').on('click', function() {
 $('nav a[href*="#"]').on('click', function() {
 $('html, body').animate(keyframes:{
   scrollTop: $($(this).attr('href')).offset().top - 100
-}, options:2000);
+}, 2000);
 });
 
 $('#up').on('click', function() {
   $('html, body').animate(keyframes:{
     scrollTop: 0
-  }, options:2000);
+  }, 2000);
   });
 
   AOS.init({
