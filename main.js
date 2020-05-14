acab195c6e8042cd85d1d32f7d763c3156cf1236
costindumitrescu <dumitrescucostin20@gmail.com>
2020-05-14 13:45:57 -0400

    finished some more

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ $('.top-nav .nav-link').on('click', function() {
 });
 
 $('nav a[href*="#"]').on('click', function() {
-$('html, body').animate(keyframes:{
+$('html, body').animate({
   scrollTop: $($(this).attr('href')).offset().top - 100
 }, 2000);
 });
 
 $('#up').on('click', function() {
-  $('html, body').animate(keyframes:{
+  $('html, body').animate({
     scrollTop: 0
   }, 2000);
   });
